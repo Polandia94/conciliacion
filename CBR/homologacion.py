431aada90695dc2f9bf97abf7a813f92ff521791
Polandia94 <pablo22estevez@gmail.com>
2021-08-19 13:00:12 -0300

    ultima

--- CBR/homologacion.py
+++ CBR/homologacion.py
@@ -33,7 +33,7 @@ def HomologacionBcoBOD(request, aCbrenc, data, saldobcoanterior):
         try:
             dataBco=pd.read_csv( str( aCbrenc.archivobco ), delimiter="|", header=None, index_col=False, names = list(range(0,11)) )
         except:
-            dataBco=pd.read_csv( "../media/" + str( aCbrenc.archivobco ), delimiter="|", header=None, index_col=False, names = list(range(0,11)) )
+            dataBco=pd.read_csv( "/home/pestevez/aplicacion/conciliacion/media/" + str( aCbrenc.archivobco ), delimiter="|", header=None, index_col=False, names = list(range(0,11)) )
         fallo = False
         print(len( dataBco ))
         sobreescribir=request.POST['sobreescribir']
@@ -203,7 +203,7 @@ def HomologacionErpGAL(request, aCbrenc, data, saldoerpanterior):
         try:
             dataErp=pd.read_csv( str( aCbrenc.archivoerp ), header=None, delimiter = "|", index_col=False, names = list(range(0,11)))
         except:
-            dataErp=pd.read_csv( "../media/" + str( aCbrenc.archivoerp ), header=None, delimiter = "|", index_col=False, names = list(range(0,11)))
+            dataErp=pd.read_csv( "/home/pestevez/aplicacion/conciliacion/media/" + str( aCbrenc.archivoerp ), header=None, delimiter = "|", index_col=False, names = list(range(0,11)))
         iniciado = False
         pausa = False
         fallo = False
